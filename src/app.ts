2e5baf1682ed9eaea01cce9ee2ed635ccadce189
krititwa Ray <krititwaray97@gmail.com>
2024-09-14 21:26:22 +0530

    create app_routing

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,9 @@ import { Config } from './config/config';
 const config = new Config();
 config.connectMongoDB()
 
+import { app_router } from './app_routing';
+app.use('/v1', app_router)
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
